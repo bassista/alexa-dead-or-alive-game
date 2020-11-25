ea2798db47949b0206b905946372f0eed61f34b0
Simon Prickett <simon@crudworks.org>
2020-11-25 20:12:42 +0000

    Added more pieces the game will need.

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ const { getRedisClient, getKeyName } = require('./redis');
 const redis = getRedisClient();
 const EXPIRY_TIME = 60 * 60 * 24;
 
+const CELEBRITIES_KEY = 'celebrities';
+
 const getCelebStatus = async (celeb) => {
   // Check for cached result.
   const celebStatusKey = getKeyName(celeb, 'status');
@@ -19,9 +21,53 @@ const getCelebStatus = async (celeb) => {
   return status;
 };
 
+const setupNewGame = async (userId, numCelebs) => {
+  // TODO this should move to Lua...
+  const celebritySetKeyName = getKeyName(CELEBRITIES_KEY);
+  const maxPossibleCelebs = await redis.scard(celebritySetKeyName);
+
+  if (numCelebs < 1 || numCelebs > maxPossibleCelebs) {
+    return false;
+  }
+
+  const userSetKeyName = getKeyName(userId, CELEBRITIES_KEY);
+  await redis.del(userSetKeyName);
+
+  let numUserCelebs = 0;
+
+  do {
+    let randomCeleb = await redis.srandmember(celebritySetKeyName);
+    await redis.sadd(userSetKeyName, randomCeleb);
+    numUserCelebs = await redis.scard(userSetKeyName);
+  } while (numUserCelebs < numCelebs);
+
+  return true;
+};
+
+const getRandomCeleb = async (userId) => {
+  const randomCeleb = await redis.spop(getKeyName(userId, CELEBRITIES_KEY));
+  return randomCeleb;
+};
+
 const run = async () => {
-  const status = await getCelebStatus('Diego_Maradona');
-  console.log(status);
+  //const status = await getCelebStatus('Diego_Maradona');
+  //console.log(status);
+  const gameEstablished = await setupNewGame(998, 10);
+
+  if (gameEstablished) {
+    let currentCeleb;
+
+    do {
+      currentCeleb = await getRandomCeleb(998);
+      if (currentCeleb) {
+        const celebStatus = await getCelebStatus(currentCeleb);
+        console.log(celebStatus);
+      }
+    } while (currentCeleb);
+
+    console.log('All done!');
+  }
+
   redis.quit();
 };
 
